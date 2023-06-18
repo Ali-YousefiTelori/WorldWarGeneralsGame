19ce649e7b3f26dcfc3f537d358051deb2e6c423
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-18 16:51:26 +0330

    add some tests

---
+++ src/WorldWarGeneralsGame.Tests/Connectors/WarAttacks/NormalWarAttackUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldWarGeneralsGame.Components.Damages.HumanWeapons;
+using WorldWarGeneralsGame.Connectors.WarAttacks;
+using WorldWarGeneralsGame.Interfaces;
+using WorldWarGeneralsGame.Units.Persons;
+
+namespace WorldWarGeneralsGame.Tests.Connectors.WarAttacks
+{
+    public class NormalWarAttackUnitTest
+    {
+        [Fact]
+        public async Task ShootSolidersByKalashnikovGunTest()
+        {
+            Soldier soldier1 = new Soldier();
+            Soldier soldier2 = new Soldier();
+            NormalWarAttack warAttack = new NormalWarAttack();
+            var soldierHealth = soldier1.Components.Where(x => x is IHealth).Cast<IHealth>().Sum(x => x.Amount);
+            foreach (KalashnikovGun item in soldier1.Weapons.Where(x => x is KalashnikovGun))
+            {
+                for (int i = 0; i < soldierHealth; i++)
+                {
+                    await warAttack.Shoot(item, soldier2);
+                    if (item.IsNeedsReload())
+                        await item.Reload();
+                    if (soldier2.IsDead)
+                        break;
+                }
+            }
+            Assert.True(soldier2.IsDead);
+        }
+    }
+}
---
+++ src/WorldWarGeneralsGame.Tests/Units/SoldierUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using WorldWarGeneralsGame.Components.Damages.HumanWeapons;
+using WorldWarGeneralsGame.Units.Persons;
+
+namespace WorldWarGeneralsGame.Tests.Units
+{
+    public class SoldierUnitTest
+    {
+        [Fact]
+        public async Task ShootKalashnikovGunTest()
+        {
+            Soldier soldier = new Soldier();
+            int realodCount = 0;
+            foreach (KalashnikovGun item in soldier.Weapons.Where(x => x is KalashnikovGun))
+            {
+                int shootCount = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    var shootResult = await item.Shoot();
+                    shootCount += item.DecreasePerShoot;
+                    if (item.IsNeedsReload())
+                    {
+                        shootResult = await item.Shoot();
+                        Assert.Equal(0, shootResult);
+                        Assert.Equal(item.HitCount, shootCount);
+                        await item.Reload();
+                        shootCount = 0;
+                        realodCount++;
+                    }
+                }
+            }
+            Assert.Equal(3, realodCount);
+        }
+    }
+}
